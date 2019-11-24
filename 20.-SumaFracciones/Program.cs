dfaa5e9ce7d4f7e72ca9c4d06164924d4fe8f24e
Flor Campos Flores <u201601859@upc.edu.pe>
2019-11-24 13:32:52 -0500

    suma fracciones

---
+++ 20.-SumaFracciones/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._SumaFracciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escriba un programa que calcule e imprima el valor de la sumatoria de los N primeros
+              términos de la serie 1 + 1/2 + 1/4 + 1/8 + 1/16+...*/
+
+            Console.Write("N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int i = 1; //indice : i
+            double sum = 0;
+            double d = 1;
+
+            while ( i <= n)
+            {
+                double numerador = 1;
+                double denominador = d;
+
+                sum += numerador / denominador;
+
+                d *= 2;
+                ++i;
+            }
+
+            Console.WriteLine("Suma: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
