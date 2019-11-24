c22b90ffee8aaf19bcbe91e4c9c0bf6f91d101b9
Flor Campos Flores <u201601859@upc.edu.pe>
2019-11-24 10:34:53 -0500

    While03

---
+++ While03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While03
+{
+    class Program
+    {
+        //pide valores hasta que se ingrese cero
+        //finalmente imprimir la suma de todos
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            bool sigue = true;
+
+            while (sigue)
+            {
+                Console.Write("Valor [0==FIN]: ");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                sum += x;
+
+                //Primera forma
+                //if (x == 0)
+                //{
+                //    sigue = false;
+                //}
+
+                //Segunda forma
+                //sigue = (x == 0) ? false : true;
+
+                //Tercer forma
+                sigue = !(x == 0);
+
+            }
+
+            Console.WriteLine("Suma: " + sum);
+            Console.ReadKey();
+
+        }
+    }
+}
